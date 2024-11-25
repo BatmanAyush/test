d3fb6aee4ce659f2fa6c6f1c6c0557a041b7c522
ayushsharma <asbro3886@gmail.com>
2024-11-25 12:18:23 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import LandingPage from './LandingPage';
 import ThankYouPage from './ThankYouPage';
-import useGTMPageView from './useGTMPageView'; // Import the custom hook
+
+
+
 
 const App = () => {
   return (
-    <Router>
-      <PageViewTracker>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/thankyou" element={<ThankYouPage />} />
-        </Routes>
-      </PageViewTracker>
+    <div>
+ <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/thankyou" element={<ThankYouPage/>} />
+                
+
+      </Routes>
+
     </Router>
-  );
-};
+    </div>
+  )
+}
 
-const PageViewTracker: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  useGTMPageView(); // Apply the hook to track page views
-  return <>{children}</>;
-};
-
-export default App;
+export default App
